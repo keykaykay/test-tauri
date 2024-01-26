8c279569e85bd86c94943924a94714d148c7a868
keykay <85695157@qq.com>
2024-01-26 08:41:56 +0800

    fix: 修复问题

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,62 +1,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// use playwright::Playwright;
 
-// #[tauri::command]
-// fn greet(name: &str) -> String {
-//     format!("Hello, {}! You've been greeted from Rust!", name)
-// }
+use tauri::{Manager, Window};
 
-// #[tauri::command]
-// async fn pw() -> Result<(), playwright::Error> {
-//     let playwright = Playwright::initialize().await?;
-//     playwright.prepare()?;
-//     let chromium = playwright.chromium();
-//     let browser = chromium.launcher().launch().await?;
-//     let context = browser.context_builder().build().await?;
-//     let page = context.new_page().await?;
-//     page.goto_builder("https://www.baidu.com").goto().await?;
-
-//     page.eval("() => alert('Hello World!')").await?;
-//     Ok(())
-// }
-
-use tauri::Manager;
-
+#[tauri::command]
+fn handle_window(window: Window, payload: &str) {
+    let loading_window = window.get_window("loading").expect("loading 窗口不存在");
+    loading_window.close().unwrap();
+    let qzd_window = window.get_window("qizhidao").expect("企知道 窗口不存在");
+    qzd_window.show().expect("企知道 窗口显示失败");
+    qzd_window.eval(payload).expect("企知道 窗口执行脚本失败");
+}
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            let window = app.get_window("main").unwrap();
-            window.eval("
-                function sleep(ms) {
-                    return new Promise(resolve => setTimeout(resolve, ms))
-                }
-                setTimeout(async () => {
-                    const spanEl = document.querySelector('#__layout > div > div.login-container.layout-content-main > div.main-blk > div > div > div > div > div > div > div.tab-box > div:nth-child(2) > span');
-                    spanEl.click();
-                    await sleep(500);
-                    const inputEl = document.querySelector('#__layout > div > div.login-container.layout-content-main > div.main-blk > div > div > div > div > div > div > div.form-blk > form > div:nth-child(1) > div > div > input');
-                    inputEl.value = '';
-                    await sleep(500);
-                    inputEl.value = '13717955328';
-                    inputEl.dispatchEvent(new Event('input'));
-                    await sleep(500);
-                    const passwordEl = document.querySelector('#__layout > div > div.login-container.layout-content-main > div.main-blk > div > div > div > div > div > div > div.form-blk > form > div.el-form-item.psput.is-required > div > div > input');
-                    passwordEl.value = 'cps63072281';
-                    passwordEl.dispatchEvent(new Event('input'));
-                    await sleep(500);
-                    const ruleEl = document.querySelector('#__layout > div > div.login-container.layout-content-main > div.main-blk > div > div > div > div > div > div > div.form-blk > form > div.note-line > span > span > div > label > span > span');
-                    ruleEl.click();
-                    await sleep(500);
-                    const loginEl = document.querySelector('#__layout > div > div.login-container.layout-content-main > div.main-blk > div > div > div > div > div > div > div.form-blk > form > div.el-form-item.btn-line > div > button');
-                    loginEl.click();
-                }, 1500);
-            ").unwrap();
+            #[cfg(debug_assertions)]
+            {
+                let loading_window = app.get_window("loading").unwrap();
+                loading_window.open_devtools();
+            }
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![handle_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,149 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { readTextFile, BaseDirectory, writeTextFile } from '@tauri-apps/api/fs'
+import { invoke } from '@tauri-apps/api'
+import { Input, message, Modal } from 'ant-design-vue'
+const showModal = ref(false)
+const info = ref({
+  username: '',
+  password: ''
+})
+const INFO_PATH = 'info.txt'
+async function handleInputInfo() {
+  if (info.value.username === '' || info.value.password === '') {
+    message.warning({
+      content: '请输入用户名和密码',
+      duration: 2
+    })
+    return
+  }
+  await writeTextFile(
+    INFO_PATH,
+    `${info.value.username}\n${info.value.password}`,
+    {
+      dir: BaseDirectory.App
+    }
+  )
+  showModal.value = false
+  handleSendEvent()
+}
+
+function handleLoading() {
+  const loadingEl = document.querySelector(
+    '.app-loading>.app-loading-wrap>.app-loading-title>.loading'
+  ) as HTMLDivElement
+  let loadingContent = ''
+  const loadingText: string = '企知道登录中...'
+  for (let index = 0; index < loadingText.length; index++) {
+    const element = loadingText[index]
+    loadingContent += `<div class="l-${index} letter">${element}</div>`
+  }
+  loadingEl.innerHTML = loadingContent
+}
+
+async function handleSendEvent() {
+  try {
+    const content = await readTextFile(INFO_PATH, {
+      dir: BaseDirectory.App,
+      append: true
+    })
+    const info = content.split('\n')
+    if (info.length < 2) {
+      showModal.value = true
+    } else {
+      invoke('handle_window', {
+        payload: `
+        document.addEventListener('DOMContentLoaded', () => {
+          const accessTokenCookie = document.cookie.split(';').filter(item => item.includes('accessToken'))
+          console.log({accessTokenCookie})
+          if (accessTokenCookie.length > 0) {
+            const accessToken = accessTokenCookie[0].split('=')[1]
+            if (accessToken) {
+              window.location.href = 'https://www.qizhidao.com'
+            } else {
+              setTimeout(() => {
+                run()
+              }, 500)
+            }
+          } else {
+            setTimeout(() => {
+              run()
+            }, 500)
+          }
+        })
+        async function run() {
+          function sleep(ms) {
+            return new Promise((resolve) => setTimeout(resolve, ms))
+          }
+          const spanEl = document.querySelector(
+            '#__layout > div > div.login-container.layout-content-main > div.main-blk > div > div > div > div > div > div > div.tab-box > div:nth-child(2) > span'
+          )
+          spanEl.click()
+          await sleep(500)
+          const inputEl = document.querySelector(
+            '#__layout > div > div.login-container.layout-content-main > div.main-blk > div > div > div > div > div > div > div.form-blk > form > div:nth-child(1) > div > div > input'
+          )
+          inputEl.value = ''
+          await sleep(500)
+          inputEl.value = '${info[0]}'
+          inputEl.dispatchEvent(new Event('input'))
+          await sleep(500)
+          const passwordEl = document.querySelector(
+            '#__layout > div > div.login-container.layout-content-main > div.main-blk > div > div > div > div > div > div > div.form-blk > form > div.el-form-item.psput.is-required > div > div > input'
+          )
+          passwordEl.value = '${info[1]}'
+          passwordEl.dispatchEvent(new Event('input'))
+          await sleep(500)
+          const ruleEl = document.querySelector(
+            '#__layout > div > div.login-container.layout-content-main > div.main-blk > div > div > div > div > div > div > div.form-blk > form > div.note-line > span > span > div > label > span > span'
+          )
+          ruleEl.click()
+          await sleep(500)
+          const loginEl = document.querySelector(
+            '#__layout > div > div.login-container.layout-content-main > div.main-blk > div > div > div > div > div > div > div.form-blk > form > div.el-form-item.btn-line > div > button'
+          )
+          loginEl.click()
+        }
+        `
+      })
+    }
+  } catch (error) {
+    showModal.value = true
+  }
+}
+
+onMounted(async () => {
+  handleLoading()
+})
+
+document.addEventListener('DOMContentLoaded', async () => {
+  handleSendEvent()
+})
+</script>
 <template>
-  <!-- <iframe
-    id="meframe"
-    style="width: 100%; height: 100%"
-    src="https://www.qizhidao.com/login"
-    frameborder="0"
-  ></iframe> -->
+  <div class="app-loading">
+    <div v-if="!showModal" class="app-loading-wrap">
+      <div class="app-loading-title">
+        <div class="loading">企知道登录中...</div>
+      </div>
+    </div>
+    <Modal
+      v-model:open="showModal"
+      title="登录信息"
+      @ok="handleInputInfo"
+      @cancel="
+        () => {
+          showModal = true
+        }
+      "
+      :keyboard="false"
+      :maskClosable="false"
+      :closable="false"
+    >
+      <Input v-model:value="info.username" placeholder="请输入用户名" />
+      <Input v-model:value="info.password" placeholder="请输入密码" />
+    </Modal>
+  </div>
 </template>
 <style>
 html,
@@ -16,5 +154,112 @@ body,
   margin: 0;
   padding: 0;
   overflow: hidden;
+}
+
+.app-loading {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #fff;
+}
+.dark .app-loading {
+  background-color: #1a202c;
+}
+.app-loading .app-loading-wrap {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  display: flex;
+  transform: translate3d(-50%, -50%, 0);
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.app-loading-title {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  font-weight: 700;
+}
+.app-loading-title .loading {
+  margin-top: 10px;
+  display: flex;
+}
+.app-loading-title img {
+  width: 60px;
+  height: 60px;
+  margin-bottom: 5px;
+  animation-name: loading;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  animation-direction: linear;
+  animation-delay: 1.8s;
+}
+.loading .letter {
+  animation-name: loading;
+  animation-duration: 1.6s;
+  animation-iteration-count: infinite;
+  animation-direction: linear;
+  float: left;
+  font-size: 30px;
+  color: #777;
+}
+.loading .l-0 {
+  animation-delay: 0.48s;
+}
+.loading .l-1 {
+  animation-delay: 0.6s;
+}
+.loading .l-2 {
+  animation-delay: 0.72s;
+}
+.loading .l-3 {
+  animation-delay: 0.84s;
+}
+.loading .l-4 {
+  animation-delay: 0.96s;
+}
+.loading .l-5 {
+  animation-delay: 1.08s;
+}
+.loading .l-6 {
+  animation-delay: 1.2s;
+}
+.loading .l-7 {
+  animation-delay: 1.32s;
+}
+.loading .l-8 {
+  animation-delay: 1.44s;
+}
+.loading .l-9 {
+  animation-delay: 1.56s;
+}
+.loading .l-10 {
+  animation-delay: 1.68s;
+}
+.loading .l-11 {
+  animation-delay: 1.8s;
+}
+.loading .l-12 {
+  animation-delay: 1.92s;
+}
+.loading .l-13 {
+  animation-delay: 2.04s;
+}
+.loading .l-14 {
+  animation-delay: 2.16s;
+}
+@keyframes loading {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
